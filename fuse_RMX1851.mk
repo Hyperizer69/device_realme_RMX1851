73dbfdafeedfc41eec090f1a0d5a3dcba8bc1b14
TheBeast <xootkama@gmail.com>
2022-01-19 02:01:16 +0000

    Add

---
+++ fuse_RMX1851.mk
@@ -0,0 +1,37 @@
+#
+# Copyright (C) 2019 The LineageOS Project
+#
+# SPDX-License-Identifer: Apache-2.0
+#
+
+# Inherit some common SparkOS stuff.
+$(call inherit-product, vendor/fuse/config/common_full_phone.mk)
+
+# Inherit from RMX1851 device.
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_DEVICE := RMX1851
+PRODUCT_NAME := fusion_RMX1851
+PRODUCT_BRAND := Realme
+PRODUCT_MODEL := Realme 3 Pro
+PRODUCT_MANUFACTURER := Realme
+
+PRODUCT_GMS_CLIENTID_BASE := android-realme
+
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="raven-user 12 SQ1D.220105.007 8030436 release-keys"
+
+BUILD_FINGERPRINT := "google/raven/raven:12/SQ1D.220105.007/8030436:user/release-keys"
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME="RMX1851" \
+    TARGET_DEVICE="RMX1851"
+
+# Gapps Build
+#WITH_GMS := true
+
